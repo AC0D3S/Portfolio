2626a280f46f2d9b925946aafd1cde2eaa5c4533
Armand <armandcodes@gmail.com>
2023-04-10 11:58:39 +0200

    Created a typing effect aswell as animation at landing page

---
+++ autoTyper.js
@@ -0,0 +1,5 @@
+var typed = new Typed("#typing-text", {
+  strings: ["I'm a <b class='color'>Professional Problem Solver</b> when it comes to the frontend of website development. I have a <b class='color'>burning passion</b> for designing websites that will change the internet game."],
+  typeSpeed: 30,
+  showCursor: false,
+});
